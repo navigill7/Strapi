e7a7a2f12524f1cf4fdce0a275868ebc6f61c2a2
Navinder Gill <126389118+navigill7@users.noreply.github.com>
2025-06-18 13:45:41 +0530

    adding task definition resource block for ecs

---
+++ Deployment_infra/ECS/task_definition.tf
@@ -0,0 +1,40 @@
+resource "aws_ecs_task_definition" "TD" {
+  family                   = "medusa-service"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = "4096"
+  memory                   = "8192"
+  execution_role_arn       = var.execution_role_arn
+  container_definitions = jsonencode([
+    {
+      name  = "strapi"
+      image = "gillnavi/strapi-nani:latest"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 1337
+          hostPort      = 1337
+          protocol      = "tcp"
+        }
+      ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          "awslogs-group"         = "/ecs/strapi-service"
+          "awslogs-region"        = "us-east-1"
+          "awslogs-stream-prefix" = "strapi"
+          "awslogs-create-group"  = "true"
+        }
+      }
+    }
+  ])
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+
+
+
+
