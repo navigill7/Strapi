154a8cdd8f1427071f5416c2a6cac826830e54f5
Navinder Gill <126389118+navigill7@users.noreply.github.com>
2025-06-19 16:55:27 +0530

    adding container insights settings in ecs cluster for cloudwatch setup

--- Deployment_infra/ECS/main.tf
+++ Deployment_infra/ECS/main.tf
@@ -1,8 +1,12 @@
 resource "aws_ecs_cluster" "strapi-cluster" {
   name = "strapi-deployment-cluster"
+
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+
   tags = {
     name = "strapi-deployment-cluster"
   }
 }
-
-
