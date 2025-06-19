d1c8c467d4f06758376435a32aed521e9d38a817
Navinder Gill <126389118+navigill7@users.noreply.github.com>
2025-06-19 16:59:40 +0530

    adding logconfiguration for cloudwatch metrics

--- Deployment_infra/ECS/task_definition.tf
+++ Deployment_infra/ECS/task_definition.tf
@@ -5,10 +5,11 @@ resource "aws_ecs_task_definition" "TD" {
   cpu                      = "4096"
   memory                   = "8192"
   execution_role_arn       = var.execution_role_arn
+
   container_definitions = jsonencode([
     {
-      name  = "strapi"
-      image = "gillnavi/strapi:f46644652b39301ac54f76831bc6551dc0d1f59e"
+      name      = "strapi"
+      image     = "gillnavi/strapi:f46644652b39301ac54f76831bc6551dc0d1f59e"
       essential = true
       portMappings = [
         {
@@ -20,10 +21,9 @@ resource "aws_ecs_task_definition" "TD" {
       logConfiguration = {
         logDriver = "awslogs"
         options = {
-          "awslogs-group"         = "/ecs/strapi-service"
-          "awslogs-region"        = "us-east-1"
-          "awslogs-stream-prefix" = "strapi"
-          "awslogs-create-group"  = "true"
+          awslogs-group         = "/ecs/strapi"
+          awslogs-region        = "us-east-1"
+          awslogs-stream-prefix = "strapi"
         }
       }
     }
@@ -33,8 +33,3 @@ resource "aws_ecs_task_definition" "TD" {
     create_before_destroy = true
   }
 }
-
-
-
-
-
