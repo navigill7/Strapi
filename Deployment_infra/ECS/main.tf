00f12a0bc4343be2f5e9244b2a1ac62ef103b515
Navinder Gill <126389118+navigill7@users.noreply.github.com>
2025-06-19 16:57:19 +0530

    adding cloudwatch log group for colleting the logs here

--- Deployment_infra/ECS/main.tf
+++ Deployment_infra/ECS/main.tf
@@ -10,3 +10,9 @@ resource "aws_ecs_cluster" "strapi-cluster" {
     name = "strapi-deployment-cluster"
   }
 }
+
+
+resource "aws_cloudwatch_log_group" "ecs_strapi_logs" {
+  name              = "/ecs/strapi"
+  retention_in_days = 7
+}
