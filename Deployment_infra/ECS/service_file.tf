78b52db4e273305ab638e066524f76f3af4398f2
Navinder Gill <126389118+navigill7@users.noreply.github.com>
2025-06-20 17:03:11 +0530

    updating the ecs configuartion to use fargate spot instead of fargate on-demand

--- Deployment_infra/ECS/service_file.tf
+++ Deployment_infra/ECS/service_file.tf
@@ -3,17 +3,21 @@ resource "aws_ecs_service" "strapi-cluster-service" {
   cluster         = aws_ecs_cluster.strapi-cluster.id
   task_definition = aws_ecs_task_definition.TD.arn
   desired_count   = 2
-  launch_type     = "FARGATE"
+
+  capacity_provider_strategy {
+    capacity_provider = "FARGATE_SPOT"
+    weight            = 1
+  }
+
   network_configuration {
     subnets          = var.subnet_ids
     security_groups  = var.security_group_id
-    assign_public_ip = false
+    assign_public_ip = true
   }
+
   load_balancer {
     target_group_arn = var.target_group_arn
     container_name   = "strapi"
     container_port   = 1337
   }
 }
-
-
