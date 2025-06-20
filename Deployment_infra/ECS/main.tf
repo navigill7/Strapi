7a47943e71e9befaf93bf5fc201cbc63cb48b927
Navinder Gill <126389118+navigill7@users.noreply.github.com>
2025-06-20 17:01:36 +0530

    Update main.tf

--- Deployment_infra/ECS/main.tf
+++ Deployment_infra/ECS/main.tf
@@ -5,9 +5,19 @@ resource "aws_ecs_cluster" "strapi-cluster" {
     name  = "containerInsights"
     value = "enabled"
   }
-
   tags = {
     name = "strapi-deployment-cluster"
+  }
+}
+
+resource "aws_ecs_cluster_capacity_providers" "strapi-cluster-capacity" {
+  cluster_name = aws_ecs_cluster.strapi-cluster.name
+
+  capacity_providers = ["FARGATE", "FARGATE_SPOT"]
+
+  default_capacity_provider_strategy {
+    capacity_provider = "FARGATE_SPOT"
+    weight            = 1
   }
 }
 
@@ -16,3 +26,7 @@ resource "aws_cloudwatch_log_group" "ecs_strapi_logs" {
   name              = "/ecs/strapi"
   retention_in_days = 7
 }
+
+
+
+
