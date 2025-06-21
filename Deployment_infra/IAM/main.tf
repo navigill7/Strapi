a2e96906b998bd2711a94ec712712bbb4dc1c834
Navinder Gill <126389118+navigill7@users.noreply.github.com>
2025-06-21 17:17:19 +0530

    attaching policy to ecs lambda role

--- Deployment_infra/IAM/main.tf
+++ Deployment_infra/IAM/main.tf
@@ -101,3 +101,9 @@ resource "aws_iam_policy" "ecs_cleanup_policy" {
   })
 }
 
+
+resource "aws_iam_role_policy_attachment" "ecs_cleanup_attach" {
+  role       = aws_iam_role.ecs_cleanup_lambda_role.name
+  policy_arn = aws_iam_policy.ecs_cleanup_policy.arn
+}
+
