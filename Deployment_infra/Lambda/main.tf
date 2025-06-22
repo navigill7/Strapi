bc5b8cbc1f4b8f912ff65b0fe347dd4929b210d5
Navinder Gill <126389118+navigill7@users.noreply.github.com>
2025-06-22 16:55:07 +0530

    creating lamda fucntion for ecs cleanup

---
+++ Deployment_infra/Lambda/main.tf
@@ -0,0 +1,23 @@
+resource "aws_lambda_function" "ecs_cleanup" {
+  function_name = "ecs-cleanup"
+
+  filename         = "ecs_cleanup.zip"
+  handler          = "lambda_function.lambda_handler"
+  runtime          = "python3.12"
+  role             = var.lambda_role
+  timeout          = 60
+  memory_size      = 512
+  source_code_hash = filebase64sha256("${path.module}/../Lambda/ecs_cleanup.zip")
+
+
+  vpc_config {
+    subnet_ids         = var.private_subnet_ids
+    security_group_ids = [var.lambda_security_group_id]
+  }
+
+  environment {
+    variables = {
+      ENV = "prod"
+    }
+  }
+}
