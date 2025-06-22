1bcb66db820446c972a025c67e16d0bd41060b82
Navinder Gill <126389118+navigill7@users.noreply.github.com>
2025-06-22 11:37:27 +0000

    creating variables for cloudwatch events

--- Deployment_infra/cloudwatch/variable.tf
+++ Deployment_infra/cloudwatch/variable.tf
@@ -3,3 +3,17 @@ variable "ecs_alarm_topic_arn" {
   type        = string
   
 }
+
+
+variable "ecs_cleanup_lambda_arn" {
+  description = "ARN of the ECS cleanup Lambda function"
+  type        = string
+  
+}
+
+
+variable "lambda_name" {
+  description = "Name of the ECS cleanup Lambda function"
+  type        = string
+  
+}
