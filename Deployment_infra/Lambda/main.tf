af395b545d6bff8c2057b241de291088aa0a3d4f
Navinder Gill <126389118+navigill7@users.noreply.github.com>
2025-06-23 17:28:22 +0530

    Update main.tf

--- Deployment_infra/Lambda/main.tf
+++ Deployment_infra/Lambda/main.tf
@@ -1,16 +1,12 @@
 resource "aws_lambda_function" "ecs_cleanup" {
   function_name = "ecs-cleanup"
-
-  filename         = "ecs_cleanup.zip"
-  handler          = "lambda_function.lambda_handler"
-  runtime          = "python3.12"
-  role             = var.lambda_role
-  timeout          = 60
-  memory_size      = 512
-  s3_bucket = "lambda-script-strapi-cleanup"
-  s3_key    = "ecs_cleanup.zip"
-
-
+  s3_bucket     = "lambda-script-strapi-cleanup"
+  s3_key        = "ecs_cleanup.zip"
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.12"
+  role          = var.lambda_role
+  timeout       = 60
+  memory_size   = 512
 
   vpc_config {
     subnet_ids         = var.private_subnet_ids
