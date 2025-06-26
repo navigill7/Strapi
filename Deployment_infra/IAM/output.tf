9138f4d9a59233d785aa7935ff807c10f6b927f6
Navinder Gill <126389118+navigill7@users.noreply.github.com>
2025-06-26 19:22:33 +0530

    adding output for codedeploy

--- Deployment_infra/IAM/output.tf
+++ Deployment_infra/IAM/output.tf
@@ -5,3 +5,11 @@ output "iam-role" {
 output "ecs_cleanup_lambda_role" {
   value = aws_iam_role.ecs_cleanup_lambda_role.arn
 }
+
+
+output "codedeploy_role_arn" {
+  value       = aws_iam_role.codedeploy_role.arn
+  description = "IAM role for AWS CodeDeploy ECS deployments"
+}
+
+
