bad6be9a1d9843b9eee4a1a2de07e8176190577a
Navinder Gill <126389118+navigill7@users.noreply.github.com>
2025-06-26 19:21:43 +0530

    adding the iam role for codedeploy

--- Deployment_infra/ECS/output.tf
+++ Deployment_infra/ECS/output.tf
@@ -1,17 +1,17 @@
 output "cluster_name" {
-  value       = aws_ecs_cluster.strapi_cluster.name
+  value       = aws_ecs_cluster.strapi-cluster.name
   description = "Name of the ECS cluster for Strapi"
   
 }
 
 output "service_name" {
-  value       = aws_ecs_service.strapi_service.name
+  value       = aws_ecs_service.strapi-cluster-service.name
   description = "Name of the ECS service for Strapi"
   
 }
 
 output "task_definition_arn" {
-  value       = aws_ecs_task_definition.strapi_task_definition.arn
+  value       = aws_ecs_task_definition.TD.arn
   description = "ARN of the ECS task definition for Strapi"
   
 }
--- Deployment_infra/IAM/main.tf
+++ Deployment_infra/IAM/main.tf
@@ -104,3 +104,56 @@ resource "aws_iam_role_policy_attachment" "ecs_cleanup_attach" {
   role       = aws_iam_role.ecs_cleanup_lambda_role.name
   policy_arn = aws_iam_policy.ecs_cleanup_policy.arn
 }
+
+
+resource "aws_iam_role" "codedeploy_role" {
+  name = "strapi-codedeploy-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "codedeploy.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_policy" "codedeploy_policy" {
+  name = "strapi-codedeploy-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "ecs:UpdateService",
+          "ecs:CreateTaskSet",
+          "ecs:DeleteTaskSet",
+          "ecs:DescribeServices",
+          "ecs:DescribeTaskSets",
+          "ecs:DescribeTasks",
+          "elasticloadbalancing:DescribeListeners",
+          "elasticloadbalancing:ModifyListener",
+          "elasticloadbalancing:DescribeRules",
+          "elasticloadbalancing:ModifyRule",
+          "lambda:InvokeFunction",
+          "cloudwatch:PutMetricAlarm",
+          "cloudwatch:DescribeAlarms",
+          "cloudwatch:DeleteAlarms",
+          "s3:GetObject",
+          "iam:PassRole"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "codedeploy_attach" {
+  role       = aws_iam_role.codedeploy_role.name
+  policy_arn = aws_iam_policy.codedeploy_policy.arn
+}
